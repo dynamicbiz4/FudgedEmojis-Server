bfb4d3eb78b413c75670ba48cf5b656c5269ba2e
Nimsaramahagedara <nimsaramahagedara@gmail.com>
2023-10-01 13:57:20 +0530

    corsUpdate

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import * as process from 'process'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://main--ornate-sprite-6016b0.netlify.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false, 
+  });
   await app.listen(process.env.PORT || 3000);
 
 }
