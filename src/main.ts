1bf1308413635894e0c398c5ab75a7809e688793
Nimsaramahagedara <nimsaramahagedara@gmail.com>
2023-10-01 14:11:04 +0530

    CorsOptions

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const corsOptions: CorsOptions = {
+  origin: 'https://main--ornate-sprite-6016b0.netlify.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: false, 
+};
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://main--ornate-sprite-6016b0.netlify.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: false, 
-  });
+  app.enableCors(corsOptions);
+  // app.enableCors({
+  //   origin: 'https://main--ornate-sprite-6016b0.netlify.app',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: false, 
+  // });
   await app.listen(process.env.PORT || 3000);
 
 }
