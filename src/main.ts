d6c9493f9e07c33e9d05aac84817a4527dea1333
Nimsaramahagedara <nimsaramahagedara@gmail.com>
2023-10-05 11:09:28 +0530

    enableCorsInsideConfig

--- src/main.ts
+++ src/main.ts
@@ -12,12 +12,12 @@ const corsOptions: CorsOptions = {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(corsOptions);
-  // app.enableCors({
-  //   origin: 'https://main--ornate-sprite-6016b0.netlify.app',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   credentials: false, 
-  // });
+  //app.enableCors(corsOptions);
+  app.enableCors({
+    origin: 'https://main--ornate-sprite-6016b0.netlify.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false, 
+  });
   await app.listen(process.env.PORT || 3000);
 
 }
